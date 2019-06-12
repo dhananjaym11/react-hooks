dc6ded8f9e51fad900cf086da6e80a0322f38e45
Dhananjay Mane <dhananjaymane11@gmail.com>
2019-06-12 13:10:12 +0530

    Added formik plugin for forms

--- src/App/components/Forms/Forms.js
+++ src/App/components/Forms/Forms.js
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 export default function Forms(props) {
-    const [personName, handleNameChange] = useState('');
-    const [personAge, handleAgeChange] = useState(0);
 
-    const handleChange = (e) => {
-        if (e.target.name === 'personName') {
-            handleNameChange(e.target.value);
-        } else {
-            handleAgeChange(e.target.value);
-        }
-    };
-
-    const onSaveClick = (e) => {
-        e.preventDefault();
-        props.onSaveClick(personName, personAge);
-        handleNameChange('');
-        handleAgeChange(0);
+    const onSaveClick = (data) => {
+        props.onSaveClick(data.email, data.age);
     }
 
     const handleDeletePerson = (id) => {
@@ -25,55 +14,112 @@ export default function Forms(props) {
 
     return (
         <div className="form-page">
-            <form>
-                <div className="row">
-                    <div className="col-sm-6">
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <div className="form-group">
-                                    <label>Name</label>
-                                    <input
-                                        className="form-control"
-                                        type="text"
-                                        name="personName"
-                                        value={personName}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                            </div>
-                            <div className="col-sm-6">
-                                <div className="form-group">
-                                    <label>Age</label>
-                                    <input
-                                        className="form-control"
-                                        type="text"
-                                        name="personAge"
-                                        value={personAge}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <button type="sumbit" onClick={onSaveClick}>Save</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-6">
-                        {props.personList.map(person => (
-                            <div key={person.id}>
-                                <span>{person.name} - {person.age}</span>
-                                <button
-                                    style={{ marginLeft: '10px' }}
-                                    title="delete"
-                                    onClick={() => handleDeletePerson(person.id)}>
-                                    x</button>
-                            </div>
-                        ))}
-                    </div>
+            <div className="row">
+                <div className="col-sm-6">
+                    <Formik
+                        initialValues={{ email: '', age: '' }}
+                        onSubmit={(values, { resetForm }) => {
+                            resetForm();
+                            onSaveClick(values);
+                        }}
+                        validationSchema={
+                            Yup.object().shape({
+                                email: Yup.string()
+                                    .email()
+                                    .required('Required'),
+                                age: Yup.number()
+                                    .typeError('age must be a number')
+                                    .required('Required'),
+                            })
+                        }
+                    >
+                        {props => {
+                            const {
+                                values,
+                                isValid,
+                                touched,
+                                errors,
+                                dirty,
+                                isSubmitting,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                handleReset,
+                            } = props;
+                            return (
+                                <form onSubmit={handleSubmit}>
+                                    <div className="row">
+                                        <div className="col-sm-6">
+                                            <div className="form-group">
+                                                <label htmlFor="email" style={{ display: 'block' }}>Email</label>
+                                                <input
+                                                    id="email"
+                                                    placeholder="Enter your email"
+                                                    type="text"
+                                                    value={values.email}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    className={
+                                                        errors.email && touched.email ? 'form-control error' : 'form-control'
+                                                    }
+                                                />
+                                                {errors.email && touched.email && (
+                                                    <div className="input-feedback">{errors.email}</div>
+                                                )}
+                                            </div>
+                                        </div>
+                                        <div className="col-sm-6">
+                                            <div className="form-group">
+                                                <label htmlFor="age" style={{ display: 'block' }}>Age</label>
+                                                <input
+                                                    id="age"
+                                                    placeholder="Enter your age"
+                                                    type="text"
+                                                    value={values.age}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    className={
+                                                        errors.age && touched.age ? 'form-control error' : 'form-control'
+                                                    }
+                                                />
+                                                {errors.age && touched.age && (
+                                                    <div className="input-feedback">{errors.age}</div>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-sm-6">
+                                            <button
+                                                type="button"
+                                                className="outline"
+                                                onClick={handleReset}
+                                                disabled={!dirty || isSubmitting}
+                                            >Reset</button>
+                                            <button
+                                                type="submit"
+                                                disabled={!isValid}
+                                            >Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            );
+                        }}
+                    </Formik>
                 </div>
-            </form>
+                <div className="col-sm-6">
+                    {props.personList.map(person => (
+                        <div key={person.id}>
+                            <span>{person.name} - {person.age}</span>
+                            <button
+                                style={{ marginLeft: '10px' }}
+                                title="delete"
+                                onClick={() => handleDeletePerson(person.id)}>
+                                x</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
